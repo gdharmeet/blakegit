27cb9e4fb659c528e196d7d6d9bf271e1afa35b4
gdharmeet <kaur.harmeet8702@gmail.com>
2021-01-10 02:23:30 +0530

    constraint

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var contrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,12 +20,12 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+ 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    constrainedLog= new Log(230,180,80,PI/2);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -33,8 +35,16 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
     bird = new Bird(100,100);
+ var options={
+     bodyA: bird.body,
+     bodyB: constrainedLog.body,
+     length: 50,
+     stiffness: 0.01
+ }
+ var chain=Constraint.create(options);
+ World.add(world,chain);
+    
 
 }
 
@@ -49,7 +59,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+constrainedLog.display();
     box3.display();
     box4.display();
     pig3.display();
